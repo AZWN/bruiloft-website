06e33167c9d79bbd3be2d3639e00e91bf2819cff
Aron Zwaan <aronzwaan@gmail.com>
2025-05-20 22:12:07 +0200

    Some initial preparations/brainstorming

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -6,17 +6,19 @@ if [ -z "$1" ]; then
     --workdir /srv/jekyll \
     --volume="$PWD:/srv/jekyll:Z" \
     --volume="$PWD/vendor/bundle:/usr/local/bundle:Z" \
-    --publish 4000:4000 \
-    --publish 4001:4001 \
+    --publish 7654:7654 \
+    --publish 5001:5001 \
     jekyll/builder:pages \
     ./build-in-docker.sh build
 else
   bundle install --jobs=4
   bundle exec jekyll serve \
     -H 0.0.0.0 \
+    --port 7654 \
     --trace \
     --watch \
     --force_polling \
+    --incremental \
     --livereload \
-    --livereload-port 4001
+    --livereload-port 5001
 fi
